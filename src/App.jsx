af20476ed825890235dcedf5d3cffb5b9fc6f486
rishu2604 <anand.rishu310@gmail.com>
2024-02-21 13:27:36 +0530

    "Second commit"

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import Sidebar from "./components/Sidebar";
 
 function App() {
-  return (
-    <main className="h-screen my-8 ">
-      <Sidebar />
+  const [projectState, setProjectState] = useState({
+    selectedProject: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject(){
+    setProjectState(prevState=>{
+      return{
+        ...prevState,
+        selectedProject: null
+      }
+    })
+  }
+
+  let content;
+
+  if(projectState.selectedProject===null){
+    content = <NewProject />
+  }
+  else if(projectState.selectedProject===undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+  return (          
+    <main className="h-screen my-8 flex gap-8">
+      <Sidebar onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
